edee34a58823aa381e1aacc5ac4532a9d1425bae
dxps <marius.ileana@gmail.com>
2021-03-25 00:38:09 +0200

    feat: blockchain skeleton p1

--- blockchain/src/lib.rs
+++ blockchain/src/lib.rs
@@ -1,15 +1,46 @@
-pub struct Blockchain {}
+use chrono::prelude::*;
+
+pub struct Block {
+    /// The index of the block. The first block (aka _Genesis block_) has index `1`.
+    pub index: u128,
+    /// The moment (Unix time) the block has been created.
+    pub timestamp: i64,
+    /// The proof (aka nonce) of the block.
+    pub proof: u32,
+    /// The hash of the previous block.
+    pub previous_hash: [char; 64],
+}
+
+pub struct Blockchain {
+    pub chain: Vec<Block>,
+}
 
 impl Blockchain {
     pub fn new() -> Blockchain {
-        Blockchain {}
+        let genesis_block = Block {
+            index: 1,
+            timestamp: Utc::now().timestamp_millis(),
+            proof: 1,
+            previous_hash: ['0'; 64],
+        };
+        Blockchain {
+            chain: vec![genesis_block],
+        }
     }
 }
 
+// -------------------------
+//        unit tests
+// -------------------------
+
 #[cfg(test)]
 mod tests {
+
+    use crate::Blockchain;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn creation() {
+        let bc = Blockchain::new();
+        assert_eq!(1, bc.chain[0].index);
     }
 }
